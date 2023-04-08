1a89d6d6a9e81431a5a67ad9d5bebc3d7b46a11e
Manuel Rengifo <manuelrengifov1912@gmail.com>
2023-04-08 13:06:52 -0400

    Finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const url = require('url');
 const path = require('path');
 
@@ -6,15 +6,26 @@ let mainWindow;
 let newProductWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            enableRemoteModule: true
+            }
+    });
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'views/index.html'),
         protocol: 'file',
-        slashes: true
+        slashes: true,
     }))
 
     const mainMenu = Menu.buildFromTemplate(templateMenu)
     Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed', () => {
+        app.quit();
+    })
 
 });
 
@@ -22,7 +33,12 @@ function createNewProductWindow() {
     newProductWindow = new BrowserWindow({
         width: 400,
         height: 300,
-        title: 'Add A New Product'
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            enableRemoteModule: true
+            }
     });
     newProductWindow.setMenu(null);
     newProductWindow.loadURL(url.format({
@@ -30,7 +46,15 @@ function createNewProductWindow() {
         protocol: 'file',
         slashes: true
     }))
+    newProductWindow.on('closed', () => {
+        newProductWindow = null;
+    })
 }
+
+ipcMain.on('product:new', (e, newProduct) => {
+    mainWindow.webContents.send('product:new', newProduct);
+    newProductWindow.close()
+})
 
 const templateMenu = [
     {
@@ -42,7 +66,39 @@ const templateMenu = [
                 click() {
                     createNewProductWindow();
                 }
+            },
+            {
+                label: 'Remove All Products',
+                click() {
+                    mainWindow.webContents.send('products:remove-all')
+                }
             }
         ]
+    },
+    {
+        label: 'Exit',
+        accelerator: 'Ctrl+Q',
+        click() {
+            app.quit()
+        }
     }
-]
+];
+
+/*
+if(process.env.NODE_ENV !== 'production') {
+    templateMenu.push({
+        label: 'DevTools',
+        submenu: [
+            {
+                label: 'Show/Hide Dev Tools',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
+*/
